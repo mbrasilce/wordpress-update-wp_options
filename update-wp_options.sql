5066c1da14aca07474989e96dec4852b29cc2cc0
Márcio Brasil <github@mbrasil.net>
2022-08-16 12:41:18 +0100

    Update update-wp_options.sql

--- update-wp_options.sql
+++ update-wp_options.sql
@@ -1,3 +1,5 @@
+SET SQL_SAFE_UPDATES = 0;
+
 UPDATE wp_options SET option_value = replace(option_value, 'oldurl', 'newurl') WHERE option_name = 'home' OR option_name = 'siteurl';
 UPDATE wp_posts SET guid = replace(guid, 'oldurl','newurl');
 UPDATE wp_posts SET post_content = replace(post_content, 'oldurl', 'newurl');
